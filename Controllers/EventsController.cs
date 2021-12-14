0a09f95e35648aa3957d4439d5fc3a5cc0270039
PriQA <priyanka.karpas@gmail.com>
2021-12-13 23:15:30 -0600

    Event before the exercise

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            //Events.Add("Strange Loop");
+            //Events.Add("Grace Hopper");
+            //Events.Add("Code with Pride");
+            ViewBag.Events = Events;    
+
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+
+        public IActionResult NewEvent(string eventName)
+        {
+            Events.Add(eventName);
+            return Redirect("/Events");
+        }
+
+    }
+}
